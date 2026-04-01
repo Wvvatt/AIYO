93e6da086237757e4701809893de7c44ccb022da
watt <waaatt1984@gmail.com>
2026-04-01 20:28:27 +0800

    fix: remove Tool class wrapper from EXT_TOOLS
    Revert to direct function list to fix ImportError

--- libs/aiyo/src/ext/tools/__init__.py
+++ libs/aiyo/src/ext/tools/__init__.py
@@ -1,7 +1,5 @@
 """Extension tools."""
 
-from aiyo.tools.tool import Tool
-
 from .confluence_tools import confluence_cli
 from .confluence_tools import health as confluence_health
 from .gerrit_tools import gerrit_cli
@@ -9,11 +7,7 @@ from .gerrit_tools import health as gerrit_health
 from .jira_tools import health as jira_health
 from .jira_tools import jira_cli
 
-EXT_TOOLS: list[Tool] = [
-    Tool(jira_cli, concurrent=True),
-    Tool(confluence_cli, concurrent=True),
-    Tool(gerrit_cli, concurrent=True),
-]
+EXT_TOOLS = [jira_cli, confluence_cli, gerrit_cli]
 
 # Health check functions for info() display
 EXT_TOOL_HEALTH_CHECKS = [
